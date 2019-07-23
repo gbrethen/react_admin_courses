576c724936b4c09de9bc37afd0c3aef7fe3a9546
Greg Brethen <gbrethen@Greg-Brethen.local>
2019-07-23 15:58:03 -0500

    add testing using jest framework.

--- src/redux/actions/authorActions.js
+++ src/redux/actions/authorActions.js
@@ -1,5 +1,6 @@
 import * as types from "./actionTypes";
 import * as authorApi from "../../api/authorApi";
+import { beginApiCall, apiCallError } from "./apiStatusActions";
 
 export function loadAuthorSuccess(authors) {
   return { type: types.LOAD_AUTHORS_SUCCESS, authors };
@@ -7,10 +8,12 @@ export function loadAuthorSuccess(authors) {
 
 export function loadAuthors() {
   return async function(dispatch) {
+    dispatch(beginApiCall());
     try {
       const authors = await authorApi.getAuthors();
       dispatch(loadAuthorSuccess(authors));
     } catch (error) {
+      dispatch(apiCallError(error));
       throw error;
     }
   };
